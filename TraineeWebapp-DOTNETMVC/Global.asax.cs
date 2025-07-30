88157cd356650428fc1484bb58377ccd47ce9482
Anbarasan-P <anbarasanpandikumar@gmail.com>
2025-07-30 21:48:27 +0530

    Cookie creation to decrypt the ticket

--- TraineeWebapp-DOTNETMVC/Global.asax.cs
+++ TraineeWebapp-DOTNETMVC/Global.asax.cs
@@ -5,10 +5,11 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace TraineeWebapp_DOTNETMVC
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -17,5 +18,24 @@ namespace TraineeWebapp_DOTNETMVC
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_AuthenticateRequest(Object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(authCookie.Value);
+                if (ticket != null && !ticket.Expired)
+                {
+                    string[] roles = new string[] { ticket.UserData }; // ← MUST MATCH ticket.UserData from Login
+                    var identity = new System.Security.Principal.GenericIdentity(ticket.Name);
+                    var principal = new System.Security.Principal.GenericPrincipal(identity, roles);
+                    Context.User = principal;
+                }
+            }
+        }
+
+
+
     }
 }
