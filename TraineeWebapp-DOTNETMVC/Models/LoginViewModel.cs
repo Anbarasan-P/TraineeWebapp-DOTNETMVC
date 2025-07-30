e7e8d0edd6b43df8347f76058124c5fb5796a230
Anbarasan-P <anbarasanpandikumar@gmail.com>
2025-07-30 21:44:11 +0530

    Adding LoginViewModel and Admin models

---
+++ TraineeWebapp-DOTNETMVC/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations; // This line is necessary for data annotations like [Required] and [DataType]
+
+namespace TraineeWebapp_DOTNETMVC.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required] // If I remove the system component model data annotations, it will not work and throw compile time error
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
