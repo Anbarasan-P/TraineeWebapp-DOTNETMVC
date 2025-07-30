7b0bbac305624dfcf76a205a6c5a2d32ab00ff05
Anbarasan-P <anbarasanpandikumar@gmail.com>
2025-07-30 21:47:31 +0530

    Adding authorize for roles

--- TraineeWebapp-DOTNETMVC/Controllers/TraineeController.cs
+++ TraineeWebapp-DOTNETMVC/Controllers/TraineeController.cs
@@ -4,27 +4,51 @@ using System.Web.Mvc;
 using TraineeWebapp_DOTNETMVC.Models;
 
 namespace TraineeWebapp_DOTNETMVC.Controllers
-{
-    public class TraineeController : Controller
-    {
-        TraineeDAL dataAccessLayer = new TraineeDAL();
 
+{
+    [Authorize] // Ensure that all actions require authentication // controller-wide authorization
+    public class TraineeController : Controller // TraineeController class
+    {
+        TraineeDAL dataAccessLayer = new TraineeDAL();// Data access layer instance
+
+        [Authorize(Roles = "Admin,Trainee")]
         public ActionResult Index()
         {
-            return View(dataAccessLayer.GetAllTrainees());
+            if (User.IsInRole("Admin"))
+            {
+                return View(dataAccessLayer.GetAllTrainees());
+            }
+            else
+            {
+                int traineeId = (int)Session["TraineeID"];
+                var trainee = dataAccessLayer.GetTraineeById(traineeId);
+                return View(new[] { trainee });
+            }
         }
 
+        [Authorize(Roles = "Admin,Trainee")]
         public ActionResult Details(int id)
         {
-            return View(dataAccessLayer.GetTrainee(id));
+            // Trainee can view only their own details
+            if (User.IsInRole("Trainee"))
+            {
+                int traineeId = (int)Session["TraineeID"];
+                if (traineeId != id)
+                    return new HttpUnauthorizedResult();
+            }
+
+            return View(dataAccessLayer.GetTraineeById(id));
         }
 
+        // Only Admin can create trainees
+        [Authorize(Roles = "Admin")]
         public ActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         public ActionResult Create(Trainee trainee, HttpPostedFileBase PhotoFile)
         {
             if (ModelState.IsValid)
@@ -36,50 +60,53 @@ namespace TraineeWebapp_DOTNETMVC.Controllers
                         trainee.Photo = binaryReader.ReadBytes(PhotoFile.ContentLength);
                     }
                 }
-                else {
-                    trainee.Photo = null; // Handle case where no photo is uploaded
-
-                }
                 dataAccessLayer.InsertTrainee(trainee);
                 return RedirectToAction("Index");
             }
             return View(trainee);
         }
 
+        // Only Admin can access Edit
+        [Authorize(Roles = "Admin")]
         public ActionResult Edit(int id)
         {
-            return View(dataAccessLayer.GetTrainee(id));
+            return View(dataAccessLayer.GetTraineeById(id));
         }
 
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
         public ActionResult Edit(Trainee trainee, HttpPostedFileBase PhotoFile)
         {
+            if (PhotoFile != null && PhotoFile.ContentLength > 0)
+            {
+                using (var binaryReader = new BinaryReader(PhotoFile.InputStream))
+                {
+                    trainee.Photo = binaryReader.ReadBytes(PhotoFile.ContentLength);
+                }
+            }
+            else
+            {
+                var existingTrainee = dataAccessLayer.GetTraineeById(trainee.TraineeID);
+                trainee.Photo = existingTrainee.Photo;
+            }
+
             if (ModelState.IsValid)
             {
-                if (PhotoFile != null && PhotoFile.ContentLength > 0)
-                {
-                    using (var binaryReader = new BinaryReader(PhotoFile.InputStream))
-                    {
-                        trainee.Photo = binaryReader.ReadBytes(PhotoFile.ContentLength);
-                    }
-                }
-                else
-                {
-            
-                    var existingTrainee = dataAccessLayer.GetTrainee(trainee.TraineeID);
-                    trainee.Photo = existingTrainee.Photo;
-                }
                 dataAccessLayer.UpdateTrainee(trainee);
                 return RedirectToAction("Index");
             }
             return View(trainee);
         }
 
+        // Only Admin can access Delete
+        [Authorize(Roles = "Admin")]
         public ActionResult Delete(int id)
         {
-            return View(dataAccessLayer.GetTrainee(id));
+            return View(dataAccessLayer.GetTraineeById(id));
         }
 
         [HttpPost, ActionName("Delete")]
+        [Authorize(Roles = "Admin")]
         public ActionResult DeleteConfirmed(int id)
         {
             dataAccessLayer.DeleteTrainee(id);
